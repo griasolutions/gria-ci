122a1c39fba689f9e0773fb2c7e51ed3dd787885
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-01-03 12:40:38 -0500

    Added controllers.

---
+++ src/application/controllers/DashboardController.php
@@ -0,0 +1,4 @@
+<?php
+namespace GriaCi\Application;
+
+use \GriaCi\Controller;
---
+++ src/application/controllers/ErrorController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: gfisher
+ * Date: 1/3/14
+ * Time: 12:22 PM
+ */
+
+namespace GriaCi\Application;
+
+class ErrorController extends \GriaCi\Controller\ErrorController
+{
+
+	public function route()
+	{
+		die('Nope!');
+	}
+
+} 
