84780e7dccfa6a42f01af220b10abb853804b61f
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-01-03 17:04:58 -0500

    Got rid of my silly autoloader and used composer instead for PSR-0 and PSR-4 autoloading. Changed the app & testing bootstraps accordingly.

--- src/public/index.php
+++ src/public/index.php
@@ -1,22 +1,13 @@
 <?php
 
-// define the base path
-define('APPLICATION_PATH', realpath(dirname(dirname(dirname(__FILE__)))));
-
 // set the include path
 set_include_path(implode(PATH_SEPARATOR, array(
 	get_include_path(),
-	APPLICATION_PATH,
-	APPLICATION_PATH . '/vendor',
-	APPLICATION_PATH . '/src/library',
+	dirname(dirname(dirname(__FILE__))),
 )));
-
-// initialize the auto-loader
-require 'GriaCi/Autoload/Autoloader.php';
-(new \GriaCi\Autoload\Autoloader())->init();
 
 // pull in composer dependencies
 require 'vendor/autoload.php';
 
 // dispatch the call
-(new \GriaCi\Controller\Dispatcher())->run();
+(new \GriaCi\Controller\Dispatcher(new \GriaCi\Controller\Request()))->run();
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,14 +3,8 @@
 // set the include path
 set_include_path(implode(PATH_SEPARATOR, array(
 	get_include_path(),
-	realpath(dirname(dirname(__FILE__))),
-	realpath(dirname(dirname(__FILE__)) . '/vendor'),
-	realpath(dirname(dirname(__FILE__)) . '/src/library'),
+	dirname(dirname(__FILE__)),
 )));
-
-// initialize the auto-loader
-require 'GriaCi/Autoload/Autoloader.php';
-(new \GriaCi\Autoload\Autoloader())->init();
 
 // pull in composer dependencies
 require 'vendor/autoload.php';
